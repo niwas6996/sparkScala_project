19afa376e395796be3cdfd43bffdeb5ed2b74d7b
Hemas <hemasingam2731@gmail.com>
2022-10-31 11:36:16 +0530

    test

---
+++ src/main/scala/spark_30DaysOfSpark/reverseString.scala
@@ -0,0 +1,16 @@
+package spark_30DaysOfSpark
+
+object reverseString extends App{
+
+  val str = "hai how are you"
+  var revStr = ""
+
+  for(i<- 1 to str.length) {
+    if(str.length<2) str
+    else
+      revStr+= str.substring(str.length-i,str.length-(i-1))
+
+  }
+  println(revStr)
+
+}
--- src/main/scala/spark_30DaysOfSpark/sortValueOnCountOfOccuranceOfWord.scala
+++ src/main/scala/spark_30DaysOfSpark/sortValueOnCountOfOccuranceOfWord.scala
@@ -3,18 +3,19 @@ package spark_30DaysOfSpark
 object sortValueOnCountOfOccuranceOfWord extends App {
 
   val lst = List("apple", "banana", "orange", "banana", "banana", "apple", "apple", "orange")
+//val lst = List(scala.io.StdIn.readLine())
+  def countAndSortVal(x:List[String]):Unit= {
 
-  def sortedVal(value: Map[String, Int]) =
-    value.toSeq.sortBy(_._2)
-
-  lst.flatMap(_.split(","))
+  x.flatMap(_.split(","))
     .map(word => (word, 1))
     .groupBy(_._1)
     .mapValues(m => m.map(_._2).sum)
     .toSeq
     .sortBy(_._2).reverse
-
     .foreach(println)
 
+
+  }
+  countAndSortVal(lst)
 
 }
