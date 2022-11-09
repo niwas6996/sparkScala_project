aca44a14b1955be8cc6caa07b0036437a3c52205
suresh <suresh.hadoop21@gmail.com>
2022-11-10 01:11:55 +0530

    flaten nested list

--- src/main/scala/spark_30DaysOfSpark/flattenNestedList.scala
+++ src/main/scala/spark_30DaysOfSpark/flattenNestedList.scala
@@ -4,5 +4,11 @@ object flattenNestedList extends App {
 
   val lst = List(1, 2, List(3, 4, List(5, 6)), List(7, 8))
 
+  def flat(l: List[Any]):List[Any] = l flatMap {
+
+    case ls:List[_] => flat(ls)
+    case h=> List(h)
+  }
+  println(flat(lst))
 
 }
